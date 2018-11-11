c5b083305d0ba00c0735c33c3597e724345caabd
Dare Lawal <lawaldare@gmail.com>
2018-11-11 17:30:27 +0100

    trying to fix login

--- js/signup.js
+++ js/signup.js
@@ -1,33 +1,7 @@
 
 $(document).ready(function() {
 
-
-$("#loginn").click(function(){
-		var username = $("#username").val();
-    //var email = $("#email").val();
-    var password = $("#password").val();
-		alert(username);
-		//alert(email);
-    alert(password);
-		
-		var url = 'http://localhost:3000/users';
-
-		$.get(url,
-		{
-			username: username,
-			email: email,
-      password: password,
-			
-		},
-		
-		
-		function(data, status) {
-		  alert("Data: " + data + "\nStatus: " + status);
-		});
-				
-	});
-
-
+// SIGN UP
 $("#signup").click(function(){
 		var username = $("#usernamee").val();
     var email = $("#emaill").val();
@@ -35,22 +9,50 @@ $("#signup").click(function(){
 		alert(username);
 		//alert(email);
     //alert(password);
-		
 		var url = 'http://localhost:3000/users';
-
 		$.post(url,
 		{
 			username: username,
 			email: email,
       password: password,
-			
 		},
-		
 		
 		function(data, status) {
 		  alert("Data: " + data + "\nStatus: " + status);
 		});
-				
 	});
 
+
+// LOG IN
+$('#loginn').click(function(){
+	var username = $("#username").val();
+    var password = $("#password").val();
+    var error = true;
+
+    //$('#loading').html("<img src=''>").fadeIn('fast');
+
+    $.ajax ({
+    	type: "GET",
+    	url: url,
+    	dataType: "json",
+    	success: function(data){
+    		$('#loading').fadeOut('fast');
+
+    		$.each(data, function (key, value) {
+    			if(username == value.username && password == value.password) {
+    				error = false;
+    			}
+    		});
+
+    		if (error == false) {
+    		 	document.location = "/pages/dashboard.html";
+    		} else {
+    			$('#wrapper').slideUp('slow').slideDown('slow');
+    			$('#username').val('');
+    			$('#password').val('');
+    		}
+    	}
+    });
+    return false;
+  });
 });
