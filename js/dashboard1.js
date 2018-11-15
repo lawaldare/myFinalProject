f889b44b52ad53a0747992f94192116fee3b6485
Dare Lawal <lawaldare@gmail.com>
2018-11-15 13:05:55 +0100

    updated

--- js/dashboard1.js
+++ js/dashboard1.js
@@ -49,14 +49,14 @@ $('#target').change(function(){
             var desc = item.snippet.description.substring(0, 100);
             var vid = item.id;
             $('#results').append(
-              `<div class="row" style="margin-bottom: 50px; padding: 20px; background-color: white; box-shadow:inset 15px 5px 10px 10px #FF4F5E;">
+              `<div class="row" style="margin-bottom: 50px; padding: 20px; background-color: white;">
                   <div class="col-md-12 mb-4">
 							        <article class="item" data-key="${vid}">
                         <iframe width="700" height="700" src="https://www.youtube.com/embed/${vid}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 							        </article> 
                   </div>
                   <div class="details">
-									    <h4 style="padding-left:20px; font-weight: bold;">${title}</h4>
+									    <h4 style="padding-left:20px; font-weight: bold; font-color: white;">${title}</h4>
 									    <p style="padding-left:20px;">${desc}</p>
 								  </div>
               </div>
